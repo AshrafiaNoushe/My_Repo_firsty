933e4b8df547fd6c65044d2cf255cb1fa857204e
AshrafiaNoushe <ashrafianoushe779@gmail.com>
2024-12-06 19:11:48 +0600

    Add files via upload

---
+++ ABAgain.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int t;
+cin>>t;
+while(t--){
+    int n,a,b,c;
+    cin>>n;
+    a = n/10;
+    b = n%10;
+    c = a+b;
+    cout<<c<<endl;
+}
+return(0);
+}
