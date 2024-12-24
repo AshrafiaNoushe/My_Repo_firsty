2b88b879242e4433f3239f401e6e75842373f939
Ashrafia Noushe <ashrafianoushe779@gmail.com>
2024-12-24 20:37:02 +0600

    Add files via upload

---
+++ A Again Twenty Five!.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("echoout.txt", "w", stdout);
+#endif
+    long long int n;
+    cin>>n;
+    //long long int sum;
+    if(n>=2){
+        cout<<"25"<<endl;
+    }
+    else{
+        cout<<"05"<<endl;
+    }
+    return 0;
+}
